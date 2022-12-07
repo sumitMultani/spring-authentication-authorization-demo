c87b47fb188e32ff10814295ac015358c71513a4
kchand7 <kchand@xtaascorp.com>
2022-12-07 23:52:33 +0530

    Spring basic security example

---
+++ src/test/java/com/example/springauthenticationauthorizationdemo/SpringAuthenticationAuthorizationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauthenticationauthorizationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthenticationAuthorizationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
